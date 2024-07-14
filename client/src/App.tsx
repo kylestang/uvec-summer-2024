a613933e8e48cf39fa0fc17bdb611a77133f39c3
Malcolm Seyd <malcolm@seyd.ca>
2024-07-14 10:44:47 -0700

    setup frontend state receiving

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Grid } from './data'
+import { useEffect, useReducer, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Grid, Message } from "./data";
+
+function gridReducer(state: Grid | null, action: Message): Grid | null {
+  console.debug(action)
+  switch (action.tag) {
+    case "init": {
+      const { pixels, height, width } = action;
+      return {
+        pixels,
+        height,
+        width,
+      };
+    }
+
+    case "draw": {
+      if (state === null) {
+        console.error("draw received while state null!");
+        return null;
+      }
+      const { color, offset } = action;
+      const pixels = [...state.pixels];
+      pixels[offset] = color;
+      return {
+        ...state,
+        pixels,
+      };
+    }
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [grid, setGrid] = useState<Grid | null>(null);
+  const [count, setCount] = useState(0);
+  const [grid, gridDispatch] = useReducer(gridReducer, null);
+
+  useEffect(() => {
+    const width = 100;
+    const height = 100;
+    gridDispatch({
+      tag: "init",
+      pixels: Array.from({length: width * height}).map(() => [255, 255, 255]),
+      width,
+      height,
+    });
+
+    const theirColor = [0, 255, 0] as const;
+
+    const drawTicker = setInterval(
+      () =>
+        gridDispatch({
+          tag: "draw",
+          offset: Math.floor(Math.random() * width * height),
+          color: theirColor,
+        }),
+      1000
+    );
+
+    () => {
+      clearInterval(drawTicker);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.debug(grid)
+  }, [grid])
 
   return (
     <>
@@ -32,7 +91,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/data.ts
+++ client/src/data.ts
@@ -2,12 +2,29 @@ import { z } from "zod";
 
 const u8Schema = z.number().int().gte(0).lt(255);
 
-const rgbSchema = z.tuple([u8Schema, u8Schema, u8Schema])
-export type RGB = z.infer<typeof rgbSchema>
+const rgbSchema = z.tuple([u8Schema, u8Schema, u8Schema]).readonly();
+export type RGB = z.infer<typeof rgbSchema>;
 
 export type Grid = {
-    pixels: RGB[],
-    width: number,
-    height: number,
-}
+  pixels: RGB[];
+  width: number;
+  height: number;
+};
 
+const initMsgSchema = z.object({
+  tag: z.literal("init"),
+  pixels: z.array(rgbSchema),
+  width: z.number().positive(),
+  height: z.number().positive(),
+});
+export type InitMsg = z.infer<typeof initMsgSchema>;
+
+const drawMsgSchema = z.object({
+  tag: z.literal("draw"),
+  offset: z.number().nonnegative(),
+  color: rgbSchema,
+});
+export type DrawMsg = z.infer<typeof drawMsgSchema>;
+
+export const messageSchema = z.union([initMsgSchema, drawMsgSchema]);
+export type Message = z.infer<typeof messageSchema>;
